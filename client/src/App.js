f4ff8e99b58d9abf708bfad4e18e4a5c630feb5c
loljoho <loljoho@users.noreply.github.com>
2020-12-14 15:12:40 -0500

    Create Layout component and refactor sidebar and main content

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
 
-import RacePage from './pages/RacePage';
-import OverviewPage from './pages/OverviewPage';
+import Layout from './containers/Layout';
 
 function App() {
   return (
     <>
-      <Router>
-        <Switch>
-          <Route exact path="/" render={() => (<OverviewPage />)} />
-          <Route path="/race" render={() => (<RacePage />)} />
-          <Redirect to="/" />
-        </Switch>
-      </Router>
+      <Layout />
     </>
   );
 }
